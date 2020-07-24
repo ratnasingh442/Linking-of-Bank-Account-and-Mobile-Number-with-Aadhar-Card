d4de8d1ed4fb5f45ecfd4381c34ee1b67fdf4f0b
ratnasingh442 <40440397+ratnasingh442@users.noreply.github.com>
2020-07-24 20:12:37 +0530

    Create bankbranch.sql

---
+++ bankbranch.sql
@@ -0,0 +1,5 @@
+insert into bankbranch values('punb0019100','fatehabad');
+insert into bankbranch values('hdfc0000116','faridabad');
+insert into bankbranch values('icici00001','patiala');
+insert into bankbranch values('bdbl0001014','ambala');
+insert into bankbranch values('sbin0003897','jagadhri');
