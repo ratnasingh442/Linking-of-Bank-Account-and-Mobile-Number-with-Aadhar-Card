de0f20fb9c3fafde998efbdc9dbd66b79a2e9a16
ratnasingh442 <40440397+ratnasingh442@users.noreply.github.com>
2020-07-24 20:08:52 +0530

    Create aadhar.sql

---
+++ aadhar.sql
@@ -0,0 +1,4 @@
+insert into aadhar values(322112345450,'pulkit','kamboj','male','gorakhpur',(TO_DATE('22/April/2011','DD/MON/YY')),18);
+insert into aadhar values(312112345450,'ratna','singh','female','hissar',(TO_DATE('21/April/1998','DD/MON/YY')),19);
+insert into aadhar values(242112345450,'himanshu','sharma','male','kota',(TO_DATE('25/April/1998','DD/MON/YY')),19);
+insert into aadhar values(142112345450,'anurag','kamboj','male','jagadhri',(TO_DATE('25/April/1996','DD/MON/YY')),20) ;
