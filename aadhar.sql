3750a9962ae85e8282bfb48ae829851d9ee3f9eb
ratnasingh442 <40440397+ratnasingh442@users.noreply.github.com>
2020-07-24 20:09:52 +0530

    Update aadhar.sql

--- aadhar.sql
+++ aadhar.sql
@@ -2,3 +2,4 @@ insert into aadhar values(322112345450,'pulkit','kamboj','male','gorakhpur',(TO_
 insert into aadhar values(312112345450,'ratna','singh','female','hissar',(TO_DATE('21/April/1998','DD/MON/YY')),19);
 insert into aadhar values(242112345450,'himanshu','sharma','male','kota',(TO_DATE('25/April/1998','DD/MON/YY')),19);
 insert into aadhar values(142112345450,'anurag','kamboj','male','jagadhri',(TO_DATE('25/April/1996','DD/MON/YY')),20) ;
+insert into aadhar values(562112345450,'hk','amethia','male','jaipur',(TO_DATE('25/march/1996','DD/MON/YY')),20);
