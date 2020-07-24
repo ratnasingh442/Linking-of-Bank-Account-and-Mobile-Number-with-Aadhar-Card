b2f426fdfb7a8d49260e943cfe0c84ab41580904
ratnasingh442 <40440397+ratnasingh442@users.noreply.github.com>
2020-07-24 20:15:30 +0530

    Create accounts.sql

---
+++ accounts.sql
@@ -0,0 +1,5 @@
+insert into accounts values('punb0019100',00987654321);
+insert into accounts values('hdfc0000116',00987654320);
+insert into accounts values('icici00001',00987654323);
+insert into accounts values('bdbl0001014',00987654324);
+insert into accounts values('sbin0003897',00987654325);
