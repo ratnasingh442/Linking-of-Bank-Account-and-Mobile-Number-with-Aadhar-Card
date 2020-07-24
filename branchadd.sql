9d9956f5a3d89986e0a6f65113aefafa07070a3c
ratnasingh442 <40440397+ratnasingh442@users.noreply.github.com>
2020-07-24 20:12:00 +0530

    Create branchadd.sql

---
+++ branchadd.sql
@@ -0,0 +1,5 @@
+insert into branchadd values('fatehabad','modeltown');
+insert into branchadd values('faridabad','anaj mandi');
+insert into branchadd values('patiala','22 no.');
+insert into branchadd values('ambala','new market');
+insert into branchadd values('jagadhri','old market');
