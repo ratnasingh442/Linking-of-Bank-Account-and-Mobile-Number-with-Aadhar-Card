1d0b93b2e760bd56838f28dd6a1c06484c6b671f
ratnasingh442 <40440397+ratnasingh442@users.noreply.github.com>
2020-07-24 20:14:11 +0530

    Create account.sql

---
+++ account.sql
@@ -0,0 +1,6 @@
+insert into account values('00987654321','himanshujain',1500);
+insert into account values('00987654320','himanshujain',15000);
+insert into account values('00987654323','pulkitkamboj',10);
+insert into account values('00987654324','ratnasingh',20);
+insert into account values('00987654325','ratnasingh',200000);
+insert into account values('00987654326','pulkitkamboj',10000);
