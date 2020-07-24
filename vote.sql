742ad6052b0659b84f2f2de9e79714aaac89d16b
ratnasingh442 <40440397+ratnasingh442@users.noreply.github.com>
2020-07-24 20:18:19 +0530

    Create vote.sql

---
+++ vote.sql
@@ -0,0 +1,5 @@
+insert into vote values(322112345450,0);
+insert into vote values(312112345450,0);
+insert into vote values(242112345450,0);
+insert into vote values(142112345450,0);
+insert into vote values(562112345450,0);
