8e2aab745f148b97f9349c3f9db07b2fa9618e86
ratnasingh442 <40440397+ratnasingh442@users.noreply.github.com>
2020-07-24 20:14:52 +0530

    Create accountlink.sql

---
+++ accountlink.sql
@@ -0,0 +1,5 @@
+insert into accountlink values('00987654321',322112345450);
+insert into accountlink values('00987654320',322112345450);
+insert into accountlink values('00987654323',312112345450);
+insert into accountlink values('00987654324',242112345450);
+insert into accountlink values('00987654325',142112345450);
