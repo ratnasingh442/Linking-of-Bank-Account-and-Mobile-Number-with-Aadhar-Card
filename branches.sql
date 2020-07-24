c0f23bbbb0d1cee5dd01a67aee2e547d9ad7894d
ratnasingh442 <40440397+ratnasingh442@users.noreply.github.com>
2020-07-24 20:13:29 +0530

    Create branches.sql

---
+++ branches.sql
@@ -0,0 +1,5 @@
+insert into branches values('punb0019100','pnb');
+insert into branches values('hdfc0000116','hdfc');
+insert into branches values('icici00001','icici');
+insert into branches values('bdbl0001014','bandhan');
+insert into branches values('sbin0003897','sbi');
