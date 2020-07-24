985233b05cb0a363dfcacec141d56f9b86c67d0a
ratnasingh442 <40440397+ratnasingh442@users.noreply.github.com>
2020-07-24 20:10:39 +0530

    Create mobilelink.sql

---
+++ mobilelink.sql
@@ -0,0 +1,6 @@
+insert into mobilelink values(9799670662,322112345450);
+insert into mobilelink values(9799650661,322112345450);
+insert into mobilelink values(9899650661,312112345450);
+insert into mobilelink values(9499650676,242112345450);
+insert into mobilelink values(9599650676,142112345450);
+insert into mobilelink values(9599650676,562112345450);
