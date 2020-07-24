19642889feb9f31074fdaec3bbf0602ce56c6b50
ratnasingh442 <40440397+ratnasingh442@users.noreply.github.com>
2020-07-24 20:08:06 +0530

    Create mobile.sql

---
+++ mobile.sql
@@ -0,0 +1,5 @@
+insert into mobile values(9799670662,'airtel','up') ;
+insert into mobile values(9799650661,'airtel','punjab') ;
+insert into mobile values(9899650661,'vodafone','rajsthan');
+insert into mobile values(9499650676,'bsnl','ap') ;
+insert into mobile values(9599650676,'idea','mp');
