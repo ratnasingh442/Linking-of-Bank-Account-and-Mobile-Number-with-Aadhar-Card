a70d4a308a8e158addbb656297c45977731eaad1
ratnasingh442 <40440397+ratnasingh442@users.noreply.github.com>
2020-07-24 20:17:19 +0530

    Create family_detail.sql

---
+++ family_detail.sql
@@ -0,0 +1,5 @@
+insert into familydetails values(322112345450,500000,7,'agriculture');
+insert into familydetails values(312112345450,1000000,7,'buisness');
+insert into familydetails values(242112345450,100000,8,'govtemployee');
+insert into familydetails values(142112345450,100000,8,'teaching');
+insert into familydetails values(562112345450,700000,8,'propertydealer');
